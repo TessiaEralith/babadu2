9eafa02cc12df2b6201c89bf3a2c9de6297672e0
Tessia <kira.eberzin@mail.ru>
2023-05-07 17:28:19 +1000

    Добавьте файлы проекта.

---
+++ babadu/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using read;
+
+List<User> user = read.Read.Reading();
+foreach (var item in user)
+{
+    Console.WriteLine(item.id);
+    Console.WriteLine(item.login);
+    Console.WriteLine(item.pass);
+    Console.WriteLine(item.mail);
+}
---
+++ read/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace read
+{
+    public class Read
+    {
+        public static List<User> Reading()
+        {
+            string pathToJson = @"C:\Users\Tessia\Desktop\bede.json";
+
+            string json = File.ReadAllText(pathToJson);
+
+            List<User> user = JsonSerializer.Deserialize<List<User>>(json);
+
+            return user;
+        }
+    }
+
+
+}
+public class User
+{
+    public int id { get; set; }
+    public string login { get; set; }
+    public string pass { get; set; }
+    public string mail { get; set; }
+    //public string Phone { get; set; }
+}
