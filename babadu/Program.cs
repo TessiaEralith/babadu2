8eaaede2a294c0c84668390f0e6e857dc46ee55e
Tessia <kira.eberzin@mail.ru>
2023-05-11 09:49:04 +1000

    aaa

--- babadu/Program.cs
+++ babadu/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
@@ -6,7 +7,15 @@ using System.Text.Json;
 using read;
 
 List<User> user = Read.Reading();
-AllChecks allChecks = new AllChecks();
-bool ch = allChecks.CheckingForEngaged("test");
-//bool ch = new AllChecks.CheckingForEngaged("test");
-Console.WriteLine(ch);
+Console.WriteLine(user[0].desk[0].name);
+string json= JsonSerializer.Serialize<List<User>>(user);
+Console.WriteLine(json);
+//foreach (var item in user)
+//{
+//    Console.WriteLine(item.);
+//}
+//user.ForEach(ConsoleWriteLine);
+//AllChecks allChecks = new AllChecks();
+//bool ch = allChecks.CheckingForEngaged("test");
+////bool ch = new AllChecks.CheckingForEngaged("test");
+//Console.WriteLine(ch);
--- read/read.cs
+++ read/read.cs
@@ -4,18 +4,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
 using System.IO;
 using System.Text.Json;
- static void Write(List<User> users)
+static void Write(List<User> users)
 {
     using (StreamWriter fstream = new StreamWriter(@"C:\Users\Tessia\Desktop\bede.json"))
     {
-        for (int i=0; i < users.Count; i++)
-        {
-
-        }
+        string json = JsonSerializer.Serialize<List<User>>(users);
+        fstream.WriteLine(json);
     }
 }
 namespace read
 {
+    public class User
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public List<Desk> desk { get; set; }
+    }
+    public class Desk
+    {
+        public int access { get; set; }
+        public int[] whitelist { get; set; }
+        public string name { get; set; }
+        public List<Column> column { get; set; }
+    }
+    public class Column
+    {
+        public string name { get; set; }
+        public List<Card> cards { get; set; }
+    }
+
+    public class Card
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string colour { get; set; }
+    }
     public class Read
     {
         public static List<User> Reading()
@@ -23,8 +48,10 @@ namespace read
             string pathToJson = @"C:\Users\Tessia\Desktop\bede.json";
 
             string json = File.ReadAllText(pathToJson);
+            //Console.WriteLine(json);
 
             List<User> user = JsonSerializer.Deserialize<List<User>>(json);
+            //Console.WriteLine(user[0].login);
             return user;
         }
     }
@@ -40,42 +67,26 @@ namespace read
                 {
                     check = false; return check;
                 }
-
-
-
             }
             return check;
         }
+        public string Log(string login)
+        {
+            if (CheckingForEngaged(login))
+            {
+                return ("False");
+            }
+            else
+            {
+                for (int i = 0; i < user.Count; i++)
+                {
+                    if (user[i].login == login)
+                    {
+                        return user[i].password;
+                    }
+                }
+            }
+        }
     }
-
-   
 }
-
-public class Card
-{
-    public string name;
-    public string description;
-    public string colour;
-}
-public class Column
-{
-    public string name;
-    public List<Card> cards;
-}
-public class Desk
-{
-    public int access;
-    public int[] whitelist;
-    public string name;
-    public List<Column> column;
-}
-public class User
-{
-    public int id;
-    public string login;
-    public string password;
-    public string email;
-
-    public List<Desk> desk;
-}
-
+  
